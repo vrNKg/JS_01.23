9585def46602e911fcfb43e99d04502c3018ce16
vrNKg <veronika.gamalia@mail.ru>
2023-02-02 16:07:08 +0400

    [1.2:02.02]: finish second task

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function processTheNum(result) {
     if (result === null) {
         return
     }
-    
+
     const regExp = /0+[1-9][0-9]*/
     const isNumValid = isFinite(result) && result >= 0 && Number(result) % 1 === 0 && !regExp.test(result)
     const isNotEmptyOrBlank = result !== '' && !result.includes(' ')
@@ -39,3 +39,47 @@ function processTheNum(result) {
 
 enterTheNum()
 
+
+
+// TASK 2
+
+function enterTheSymbol() {
+    let symbol = window.prompt('Enter something', '');
+    processTheSymbol(symbol)
+}
+
+function processTheSymbol(symbol) {
+    if (symbol === null) {
+            return 
+    }
+    const isLengthValid = symbol.length >= 1 && symbol.length <= 3
+    if (isLengthValid && symbol !== '' && symbol.trim()) {
+        enterNumber(symbol)
+    } else {
+        console.log('Incorrect input!');
+        enterTheSymbol()
+    }
+}
+enterTheSymbol()
+
+function enterNumber(symbol) {
+    let number = window.prompt('Enter the number', '');
+    processNumber(symbol, number)
+}
+function processNumber(symbol, number) {
+    if (number === null) {
+        return 
+    }
+
+    const regExp2 = /0+[1-9][0-9]*/
+    const isNumValid2 = isFinite(number) && number >= 0 && Number(number) % 1 === 0 && !regExp2.test(number)
+    const isNotEmptyOrBlank2 = number !== '' && !number.includes(' ')
+    const isNumInScope = (number > 0) && (number < 10)
+
+    if (isNumValid2 && isNotEmptyOrBlank2 && isNumInScope) {
+        console.log(((symbol.trim() + ' ').repeat(number) + '\n').repeat(number))   
+    } else {
+        console.log('Incorrect input!');
+        enterNumber(symbol)
+    }
+}
