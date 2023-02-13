704dc3bbd682f68276034e88be01d41af6b05dda
vrNKg <veronika.gamalia@mail.ru>
2023-02-13 18:19:38 +0400

    [4.1-3:13.02]: finish all tasks

--- index.js
+++ index.js
@@ -1,20 +1,90 @@
-Array.prototype.customFilter = function (callback, context) {
-    let newArr = []
-
-    for (let i = 0; i < this.length; i++) {
-       if (callback.call(context, this[i], i, this)) {
-            newArr.push(this[i])
-       }
-    }
-    return newArr
-}
-
-function createDebounceFunction(cb, timeout) {
-    let timerId = 0
-
-    return () => {
-    clearTimeout(timerId)
-    timerId = setTimeout(() => cb(), timeout)
+function curry(func) {
+    return function curried(...args) {
+        if (args.length >= func.length) {
+            return func.apply(this, args)
+        } else {
+            return function(...nextArgs) {
+                return curried.apply(this, [...args, ...nextArgs])
+            }
+        }
     }
 }
 
+function checkNum(value) {
+    if (!isFinite(value)) {
+        throw new Error()
+    }
+}
+
+class Calculator {
+    constructor(num1, num2) {
+        checkNum(num1)
+        checkNum(num2)
+        this.x = num1
+        this.y = num2
+    }
+    setX = (num) => {
+        checkNum(num)
+        this.x = num
+    }
+    setY = (num) => {
+        checkNum(num)
+        this.y = num
+    }
+    getSum = () => {
+        return this.x + this.y
+    }
+    getMul = () => {
+        return this.x * this.y
+    }
+    getSub = () => {
+        return this.x - this.y
+    }
+    getDiv = () => {
+        if (this.x === 0 || this.y === 0) {
+            throw new Error()
+        }
+        return this.x / this.y
+    }
+}
+
+function lessThanZero(value) {
+    if (value <= 0) {
+        throw new Error()
+    }
+}
+
+class RickAndMorty {
+    constructor() {}
+
+    getCharacter(id) {
+        checkNum(id)
+        lessThanZero(id)
+
+        return fetch(`https://rickandmortyapi.com/api/character/${id}`)
+            .then((character) => {
+                if (character.status === 404) {
+                    return null
+                }
+                return character.json()
+            })
+            .catch(() => null)
+    }
+    
+    async getEpisode(id) {
+        checkNum(id)
+        lessThanZero(id)
+
+        try {
+            let url = await fetch(`https://rickandmortyapi.com/api/episode/${id}`)
+            let episode = await url.json()
+
+            if (episode.error) {
+                return null
+            }
+            return episode
+        } catch (err) {
+            return null
+        }
+    }
+}
